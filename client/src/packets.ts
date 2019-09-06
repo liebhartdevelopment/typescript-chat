6a02d07d72b196e01f056acff9b37a37725843aa
liebhartdevelopment <nathaniel95975@gmail.com>
2019-09-06 15:25:17 -0500

    Client side

---
+++ client/src/packets.ts
@@ -0,0 +1,31 @@
+namespace Chat {
+  export enum RequestType {
+    LOGIN = "login",
+    CHAT_SEND = "chat_send"
+  }
+
+  export enum ResponseType {
+    LOGIN_RESULT = "login_result",
+    CHAT_RESULT = "chat_result"
+  }
+
+  export interface IResponsePacket {
+    readonly responseType: string;
+  }
+
+  export class ChatResponse implements IResponsePacket {
+    public readonly responseType: string;
+
+    public constructor(public userData: UserData, public content: string) {
+      this.responseType = RequestType.CHAT_RESULT;
+    }
+  }
+
+  export class LoginResponse implements IResponsePacket {
+    public readonly responseType: string;
+
+    public constructor(public success: boolean) {
+      this.responseType = ResponseType.LOGIN_RESULT;
+    }
+  }
+}
---
+++ client/src/userData.ts
@@ -0,0 +1,15 @@
+namespace Chat {
+  export class UserData {
+    public constructor(
+      public userId: number,
+      public name: string,
+      public color: string
+    ) {}
+
+    public static readonly SERVER: UserData = new UserData(
+      -1,
+      "SERVER",
+      "yellow"
+    );
+  }
+}
